cb08a1d53dd684f628ec2c998c9c2a3730d85020
teerentt <85568586+teerentt@users.noreply.github.com>
2021-06-08 15:42:26 -0700

    teerent

---
+++ rea.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '.';
+
+
+client.on('ready', () =>{
+    console.log('Rea is online!');
+});
+
+client.on('message', message =>{
+    if(message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split("  ");
+    const command = args.shift().toLocaleLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    } else if (command == 'join'){
+        message.channel.send('discord.gg/join');
+    } else if (message.content === 'hello'){
+        message.channel.send('hello there cutie!');
+    } else if(command === 'wip'){
+        message.channel.send('https://discord.gg/zJNqujCR');
+    }
+});
+
+
+
+client.login('ODQ3MTMyMzAxNDEwMzA0MDcw.YK5nbw.cfmHzslRB4jVaKQOuhWmKY2ShNk');
